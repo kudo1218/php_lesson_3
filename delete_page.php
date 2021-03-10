46094599adf830c7fa34a499fd26c24548d96bf2
fromlabo <fromlabo@fromlabonoMacBook-Air.local>
2021-03-10 21:45:18 +0900

    課題3-2/完成2

--- delete_page.php
+++ delete_page.php
@@ -1,3 +1,20 @@
+<?php
+  try{
+    //DB接続
+    $dsn = 'mysql:host=localhost;dbname=php_lesson_3;charset=utf8';
+    $user = 'user';
+    $pass = 'password';
+    $dbh = new PDO($dsn,$user,$pass);
+  }catch(PDOException $e) {
+    echo 'DB接続エラー';
+  }
+  //DBの値を削除
+  $delete_sql = 'DELETE FROM PostData WHERE id = :id';
+  $stmt = $dbh->prepare($delete_sql);
+  //↓$_POST[]に入れるのはbutton、ここ苦戦した
+  $params = array('id'=>$_POST['hidden']);
+  $stmt->execute($params);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,21 +27,4 @@
     <input type="submit" value="投稿一覧へ戻る" name="back_page">
   </form>
 </body>
-<?php
-  //DB接続
-  $dsn = 'mysql:host=localhost;dbname=php_lesson_3;charset=utf8';
-  $user = 'user';
-  $pass = 'password';
-  try{
-    $dbh = new PDO($dsn,$user,$pass);
-    //DBの値を削除
-    $delete_sql = 'DELETE FROM PostData WHERE id = :id';
-    $stmt = $dbh->prepare($delete_sql);
-    //↓$_POST[]に入れるのはbutton、ここ苦戦した
-    $params = array('id'=>$_POST['button']);
-    $stmt->execute($params);
-  }catch(PDOException $e) {
-    echo 'DB接続エラー';
-  }
-?>
 </html>
--- done_page.php
+++ done_page.php
@@ -1,3 +1,19 @@
+<?php
+  try{
+    //DB接続
+    $dsn = 'mysql:host=localhost;dbname=php_lesson_3;charset=utf8';
+    $user = 'user';
+    $pass = 'password';
+    $dbh = new PDO($dsn,$user,$pass);
+  }catch(PDOException $e) {
+    echo 'DB接続エラー';
+  }
+  //DBに値を登録
+  $insert_sql = 'INSERT INTO PostData(name,text) VALUES(:name, :text)';
+  $stmt = $dbh->prepare($insert_sql);
+  $params = array(':name'=>$_POST['user_name'], ':text'=>$_POST['user_text']);
+  $stmt->execute($params);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,21 +26,4 @@
     <input type="submit" value="投稿一覧へ戻る" name="back_page">
   </form>
 </body>
-<?php
-  //DB接続
-  $dsn = 'mysql:host=localhost;dbname=php_lesson_3;charset=utf8';
-  $user = 'user';
-  $pass = 'password';
-  try{
-    $dbh = new PDO($dsn,$user,$pass);
-    //DBに値を登録
-    $insert_sql = 'INSERT INTO PostData(name,text) VALUES(:name, :text)';
-    $stmt = $dbh->prepare($insert_sql);
-    $stmt->bindValue(':name', $_POST['user_name'], PDO::PARAM_STR);
-    $stmt->bindValue(':text', $_POST['user_text'], PDO::PARAM_STR);
-    $stmt->execute();
-  }catch(PDOException $e) {
-    echo 'DB接続エラー';
-  }
-?>
 </html>
