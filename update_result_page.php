8d2857695c64f326a40f1b9e46db7dc9557a2f7a
fromlabo <fromlabo@fromlabonoMacBook-Air.local>
2021-03-11 17:00:24 +0900

    課題3-3/完成

--- update_result_page.php
+++ update_result_page.php
@@ -1,27 +1,26 @@
 <?php
   try{
-    //DB接続
     $dsn = 'mysql:host=localhost;dbname=php_lesson_3;charset=utf8';
     $user = 'user';
     $pass = 'password';
     $dbh = new PDO($dsn,$user,$pass);
+    //対象IDのデータを更新
+    $update_sql = 'UPDATE PostData SET name = :name, text = :text WHERE id = :id';
+    $stmt = $dbh->prepare($update_sql);
+    $params = array('name'=>$_POST['user_name_edit'],'text'=>$_POST['user_text_edit'],'id'=>$_POST['update_button']);
+    $stmt->execute($params);
   }catch(PDOException $e) {
     echo 'DB接続エラー';
   }
-  //DBに値を登録
-  $insert_sql = 'INSERT INTO PostData(name,text) VALUES(:name, :text)';
-  $stmt = $dbh->prepare($insert_sql);
-  $params = array(':name'=>$_POST['user_name'], ':text'=>$_POST['user_text']);
-  $stmt->execute($params);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>投稿完了ページ</title>
+  <title>編集完了ページ</title>
 </head>
 <body>
-  <h2>投稿が完了しました。</h2>
+  <h2>編集が完了しました。</h2>
   <form action="top.php" method="post">
     <input type="submit" value="投稿一覧へ戻る" name="back_page">
   </form>
